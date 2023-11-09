d76069fdb6f31cc3dc5f108132104927c9ee6f51
fabiyuc <fabianay0024@gmail.com>
2023-11-09 11:40:51 -0300

    prueba

--- src/main/java/com/guardias/backend/repositorio/RegistroActividadRepositorio.java
+++ src/main/java/com/guardias/backend/repositorio/RegistroActividadRepositorio.java
@@ -1,7 +1,5 @@
 package com.guardias.backend.repositorio;
 
-import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +8,7 @@ import com.guardias.backend.modelo.RegistroActividad;
 @Repository
 public interface RegistroActividadRepositorio extends JpaRepository<RegistroActividad,Long>{
 
-    Optional<RegistroActividad> findById(Long id);
+    /* Optional<RegistroActividad> findById(int id); */
     boolean existsById(int id);
     
 }
