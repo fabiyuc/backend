f00c48177253d05036c51e12d4769adc109659a0
fabiyuc <fabianay0024@gmail.com>
2023-11-09 11:35:25 -0300

    corrijo errores en registro de actividades. en modelo

--- src/main/java/com/guardias/backend/repositorio/RegistroActividadRepositorio.java
+++ src/main/java/com/guardias/backend/repositorio/RegistroActividadRepositorio.java
@@ -1,14 +1,16 @@
 package com.guardias.backend.repositorio;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.guardias.backend.modelo.RegistroActividad;
 
 @Repository
-public interface RegistroActividadRepositorio extends JpaRepository<RegistroActividad,Long>{
+public interface RegistroActividadRepositorio extends JpaRepository<RegistroActividad,Integer>{
 
-    /* Optional<RegistroActividad> findById(int id); */
+    Optional<RegistroActividad> findById(int id);
     boolean existsById(int id);
     
 }
