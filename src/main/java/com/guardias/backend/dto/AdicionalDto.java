8328aca9e874efedab9f51ded270997d50214962
sebcostilla <sebcostilla@gmail.com>
2023-11-13 11:03:26 -0300

    .

--- src/main/java/com/guardias/backend/dto/AdicionalDto.java
+++ src/main/java/com/guardias/backend/dto/AdicionalDto.java
@@ -5,28 +5,13 @@ import jakarta.validation.constraints.NotBlank;
 public class AdicionalDto {
 
     @NotBlank
-    private Long id;
-    @NotBlank
     private String nombre;
 
     public AdicionalDto() {
     }
 
-    public AdicionalDto Long id,
-    String nombre)
-    {
-        this.id = id;
+    public AdicionalDto(@NotBlank String nombre) {
         this.nombre = nombre;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId
-    Long id)
-    {
-        this.id = id;
     }
 
     public String getNombre() {
