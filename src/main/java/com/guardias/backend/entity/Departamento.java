456a81a9721aa52613bed8eb274ac64b6f35afd2
fabiyuc <fabianay0024@gmail.com>
2023-11-30 19:59:08 -0300

    agrego @NoArgsConstructor a clases departamento, localidad, provincia, region

--- src/main/java/com/guardias/backend/entity/Departamento.java
+++ src/main/java/com/guardias/backend/entity/Departamento.java
@@ -16,10 +16,12 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "departamentos")
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Departamento {
 
     @Id
