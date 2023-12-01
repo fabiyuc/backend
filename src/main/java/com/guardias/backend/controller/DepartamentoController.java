7fbdb9e490b2fcf19e27d512eda8667be71254c2
sebcostilla <sebcostilla@gmail.com>
2023-12-01 11:54:44 -0300

    departamentoController listo

--- src/main/java/com/guardias/backend/controller/DepartamentoController.java
+++ src/main/java/com/guardias/backend/controller/DepartamentoController.java
@@ -9,12 +9,18 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.guardias.backend.dto.DepartamentoDto;
 import com.guardias.backend.dto.Mensaje;
 import com.guardias.backend.entity.Departamento;
 import com.guardias.backend.service.DepartamentoService;
+
+import io.micrometer.common.util.StringUtils;
 
 @RestController
 @RequestMapping("/departamento")
@@ -45,8 +51,47 @@ public class DepartamentoController {
         return new ResponseEntity(departamento, HttpStatus.OK);
     }
 
-    // TODO (DepartamentoController create)
-    // TODO (DepartamentoController update)
+    @PostMapping("/create")
+    public ResponseEntity<?> create(@RequestBody DepartamentoDto departamentoDto) {
+        if (StringUtils.isBlank(departamentoDto.getNombre()))
+            return new ResponseEntity(new Mensaje("el nombre es obligatorio"),
+                    HttpStatus.BAD_REQUEST);
+
+        Departamento departamento = new Departamento();
+        departamento.setNombre(departamentoDto.getNombre());
+        departamento.setCodigoPostal(departamentoDto.getCodigoPostal());
+        departamento.setLocalidad(departamentoDto.getLocalidad());
+        departamento.setProvincia(departamentoDto.getProvincia());
+
+        departamentoService.save(departamento);
+        return new ResponseEntity(new Mensaje("Departamento creado"), HttpStatus.OK);
+    }
+
+    @PutMapping(("/update/{id}"))
+    public ResponseEntity<?> update(@PathVariable("id") Long id, @RequestBody DepartamentoDto departamentoDto) {
+        if (!departamentoService.existsById(id))
+            return new ResponseEntity(new Mensaje("El departamento no existe"), HttpStatus.NOT_FOUND);
+        if (departamentoService.existsByNombre(departamentoDto.getNombre()) &&
+                departamentoService.getByNombre(departamentoDto.getNombre()).get().getId() == id)
+            return new ResponseEntity(new Mensaje("ese nombre ya existe"), HttpStatus.BAD_REQUEST);
+
+        if (StringUtils.isBlank(departamentoDto.getNombre()))
+            return new ResponseEntity(new Mensaje("el nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+
+        Departamento departamento = new Departamento();
+
+        if (!departamentoDto.getNombre().equals(departamento.getNombre()))
+            departamento.setNombre(departamentoDto.getNombre());
+        if (!departamentoDto.getCodigoPostal().equals(departamento.getCodigoPostal()))
+            departamento.setCodigoPostal(departamentoDto.getCodigoPostal());
+        if (!departamentoDto.getProvincia().equals(departamento.getProvincia()))
+            departamento.setProvincia(departamento.getProvincia());
+        if (!departamentoDto.getLocalidad().equals(departamento.getLocalidad()))
+            departamento.setLocalidad(departamentoDto.getLocalidad());
+
+        departamentoService.save(departamento);
+        return new ResponseEntity(new Mensaje("Departamento modificado correctamente"), HttpStatus.OK);
+    }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity<?> delete(@PathVariable("id") Long id) {
