eb281c5ff495d51b2a31f9b1b3cf426415e8e992
sebcostilla <sebcostilla@gmail.com>
2023-12-07 09:48:07 -0300

    correccion de la relacion 1 a 1 en cargo y tipo de cargo

--- src/main/java/com/guardias/backend/entity/TipoCargo.java
+++ src/main/java/com/guardias/backend/entity/TipoCargo.java
@@ -1,34 +1,33 @@
 package com.guardias.backend.entity;
 
-import java.util.Set;
-
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne; // Cambiada la importación
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity(name = "tiposcargos")
 @Data
-// @RequiredArgsConstructor
 @AllArgsConstructor
-
 @NoArgsConstructor
 public class TipoCargo {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(columnDefinition = "VARCHAR(20)")
     private String nombre;
+
     @Column(columnDefinition = "VARCHAR(80)")
     private String descripcion;
+
     private boolean eshospitalario;
 
-    @OneToMany(mappedBy = "tipoCargo")
-    private Set<Cargo> cargos;
+    @OneToOne(mappedBy = "tipoCargo")
+    private Cargo cargo;
 }
