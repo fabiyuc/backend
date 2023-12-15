a1c1d63f0c4904246507ddffed068dcf4a59a534
Luis Guerrero <beto_lage16@hotmail.com>
2023-12-15 17:33:20 -0300

    Clase Notificacion

--- src/main/java/com/guardias/backend/entity/Notificacion.java
+++ src/main/java/com/guardias/backend/entity/Notificacion.java
@@ -9,43 +9,33 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Entity(name = "cargos")
+@Entity(name = "notificaciones")
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Cargo {
+
+public class Notificacion {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(columnDefinition = "VARCHAR(30)")
-    private String nombre;
+    private String tipo;
+    @Column(columnDefinition = "VARCHAR(10)")
+    private Long posicion;
+    @Column(columnDefinition = "VARCHAR(50)")
+    private String categoria;
+
+    @JsonFormat(pattern = "dd-MM-yyyy")
+    @Column(columnDefinition = "DATE")
+    private LocalDate fechanotificacion;
+
     @Column(columnDefinition = "VARCHAR(80)")
-    private String descripcion;
-    @Column(columnDefinition = "VARCHAR(10)")
-    private String nroresolucion;
-    @Column(columnDefinition = "VARCHAR(10)")
-    private String nrodecreto;
-
-    @JsonFormat(pattern = "dd-MM-yyyy")
-    @Column(columnDefinition = "DATE")
-    private LocalDate fecharesolucion;
-
-    @JsonFormat(pattern = "dd-MM-yyyy")
-    @Column(columnDefinition = "DATE")
-    private LocalDate fechainicio;
-
-    @JsonFormat(pattern = "dd-MM-yyyy")
-    @Column(columnDefinition = "DATE")
-    private LocalDate fechafinal;
-
-    @OneToOne
-    @JoinColumn(name = "tipo_cargo_id")
-    private TipoCargo tipoCargo;
+    private String detalle;
+    @Column(columnDefinition = "VARCHAR(50)")
+    private String url;
 
 }
