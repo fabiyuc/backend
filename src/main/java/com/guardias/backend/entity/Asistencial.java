9c7c7f216fcc704c5baea747ef08605e2a1b0257
Luis Guerrero <beto_lage16@hotmail.com>
2024-01-03 12:49:03 -0300

    .

--- src/main/java/com/guardias/backend/entity/Asistencial.java
+++ src/main/java/com/guardias/backend/entity/Asistencial.java
@@ -1,9 +1,12 @@
 package com.guardias.backend.entity;
 
+import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
@@ -24,4 +27,7 @@ public class Asistencial extends Person {
     @OneToMany(mappedBy = "asistencial")
     private Set<Legajo> legajos;
 
+    @ManyToMany
+    @JoinTable(name = "asistencial_especialidad", joinColumns = @JoinColumn(name = "asistencial_id"), inverseJoinColumns = @JoinColumn(name = "especialidad_id"))
+    private Set<Especialidad> especialidades = new HashSet<>();
 }
--- src/main/java/com/guardias/backend/entity/Especialidad.java
+++ src/main/java/com/guardias/backend/entity/Especialidad.java
@@ -1,10 +1,14 @@
 package com.guardias.backend.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,5 +24,8 @@ public class Especialidad {
     private Long id;
     @Column(columnDefinition = "VARCHAR(25)")
     private String nombre;
+
+    @ManyToMany(mappedBy = "especialidades")
+    private Set<Asistencial> asistenciales = new HashSet<>();
 
 }
