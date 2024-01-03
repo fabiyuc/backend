2bfde095be5d4d52086cdc5d69cbe6689883a05b
Luis Guerrero <beto_lage16@hotmail.com>
2024-01-03 10:54:54 -0300

    rama develop luis

--- src/main/java/com/guardias/backend/controller/AsistencialController.java
+++ src/main/java/com/guardias/backend/controller/AsistencialController.java
@@ -6,9 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +20,8 @@ import com.guardias.backend.dto.AsistencialDto;
 import com.guardias.backend.dto.Mensaje;
 import com.guardias.backend.entity.Asistencial;
 import com.guardias.backend.service.AsistencialService;
+
+import io.micrometer.common.util.StringUtils;
 
 @RestController
 @RequestMapping("/asistencial")
@@ -60,15 +64,65 @@ public class AsistencialController {
 
     @PostMapping("/create")
     public ResponseEntity<?> create(@RequestBody AsistencialDto asistencialDto) {
-        /*
-         * if (StringUtils.isBlank(servicioDto.getDescripcion()))
-         * return new ResponseEntity(new Mensaje("la descripcion es obligatoria"),
-         * HttpStatus.BAD_REQUEST);
-         * if (serviceServicio.existsByDescripcion(servicioDto.getDescripcion()))
-         * return new ResponseEntity(new Mensaje("esa descripcion ya existe"),
-         * HttpStatus.BAD_REQUEST);
-         */
+
+        if (asistencialDto.getDni() < 0)
+            return new ResponseEntity<>(new Mensaje("DNI es incorrecto"), HttpStatus.BAD_REQUEST);
+
+        if (StringUtils.isBlank(asistencialDto.getApellido())) {
+            return new ResponseEntity<>(new Mensaje("El Apellido es obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+
+        if (StringUtils.isBlank(asistencialDto.getNombre())) {
+            return new ResponseEntity<>(new Mensaje("El Nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+
+        if (StringUtils.isBlank(asistencialDto.getCuil())) {
+            return new ResponseEntity<>(new Mensaje("El Cuil es obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+
         Asistencial asistencial = new Asistencial();
+
+        asistencial.setApellido(asistencialDto.getApellido());
+        asistencial.setNombre(asistencialDto.getNombre());
+        asistencial.setDni(asistencialDto.getDni());
+        asistencial.setCuil(asistencialDto.getCuil());
+        asistencial.setFechaNacimiento(asistencialDto.getFechaNacimiento());
+        asistencial.setSexo(asistencialDto.getSexo());
+        asistencial.setTelefono(asistencialDto.getTelefono());
+        asistencial.setEmail(asistencialDto.getEmail());
+        asistencial.setDomicilio(asistencialDto.getDomicilio());
+        asistencial.setEstado(asistencialDto.getEstado());
+        // asistencial.setLegajos(asistencialDto.getLegajos());
+
+        asistencialService.save(asistencial);
+        return new ResponseEntity(new Mensaje("asistencial creado"), HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<?> update(@PathVariable("id") long id, @RequestBody AsistencialDto asistencialDto) {
+        if (!asistencialService.existsById(id))
+            return new ResponseEntity(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
+
+        if (asistencialService.existsByDni(asistencialDto.getDni())
+                && asistencialService.findByDni(asistencialDto.getDni()).get().getId() != id)
+            return new ResponseEntity(new Mensaje("ya existe"), HttpStatus.BAD_REQUEST);
+
+        if (asistencialDto.getDni() < 0)
+            return new ResponseEntity<>(new Mensaje("DNI es incorrecto"), HttpStatus.BAD_REQUEST);
+
+        if (StringUtils.isBlank(asistencialDto.getApellido())) {
+            return new ResponseEntity<>(new Mensaje("El Apellido es obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+
+        if (StringUtils.isBlank(asistencialDto.getNombre())) {
+            return new ResponseEntity<>(new Mensaje("El Nombre es obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+
+        if (StringUtils.isBlank(asistencialDto.getCuil())) {
+            return new ResponseEntity<>(new Mensaje("El Cuil es obligatorio"), HttpStatus.BAD_REQUEST);
+        }
+
+        Asistencial asistencial = asistencialService.findById(id).get();
 
         asistencial.setApellido(asistencialDto.getApellido());
         asistencial.setNombre(asistencialDto.getNombre());
@@ -83,6 +137,16 @@ public class AsistencialController {
         asistencial.setLegajos(asistencialDto.getLegajos());
 
         asistencialService.save(asistencial);
-        return new ResponseEntity(new Mensaje("asistencial creado"), HttpStatus.OK);
+
+        return new ResponseEntity<>(new Mensaje("Asistencial Actualizada"), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") long id) {
+        if (!asistencialService.existsById(id))
+            return new ResponseEntity(new Mensaje("no existe"), HttpStatus.NOT_FOUND);
+        asistencialService.delete(id);
+        return new ResponseEntity<>(new Mensaje("Asistencial eliminado"), HttpStatus.OK);
+
     }
 }
