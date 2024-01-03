f46df4e83643546ab0ae5b0d6e868691bc553c78
Sebastián Rodrigo Costilla <sebcostilla@gmail.com>
2024-01-03 11:09:29 -0300

    .

--- src/main/java/com/guardias/backend/dto/AsistencialDto.java
+++ src/main/java/com/guardias/backend/dto/AsistencialDto.java
@@ -8,11 +8,13 @@ import com.guardias.backend.entity.TipoGuardia;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
 public class AsistencialDto extends PersonDto {
 
     private Set<Legajo> legajos;
