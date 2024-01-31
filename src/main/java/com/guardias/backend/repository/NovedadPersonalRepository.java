4906aa0c907802f7a09ee77a43e6de2ed3600e98
Sebastián Rodrigo Costilla <sebcostilla@gmail.com>
2024-01-31 12:20:08 -0300

    completar NovedadPersonalController

--- src/main/java/com/guardias/backend/repository/NovedadPersonalRepository.java
+++ src/main/java/com/guardias/backend/repository/NovedadPersonalRepository.java
@@ -5,6 +5,8 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.guardias.backend.entity.NovedadPersonal;
@@ -12,11 +14,10 @@ import com.guardias.backend.entity.NovedadPersonal;
 @Repository
 public interface NovedadPersonalRepository extends JpaRepository<NovedadPersonal, Long> {
 
-    // Optional<List<NovedadPersonal>> findByPersona(Long idPersona);
+    @Query("SELECT np FROM novedadesPersonales np WHERE np.persona.id = :personaId")
+    Optional<List<NovedadPersonal>> findByPersona(@Param("personaId") Long personaId);
 
     Optional<List<NovedadPersonal>> findByFechaInicio(LocalDate fechaInicio);
-
-    // boolean existsByPersona(Long idPersona);
 
     boolean existsByFechaInicio(LocalDate fechaInicio);
 }
