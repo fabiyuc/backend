d9d8991f3c785863aca7edb8509131ed79f6316d
sebcostilla <sebcostilla@gmail.com>
2024-02-19 10:18:29 -0300

    feriado funcionando con las funciones de validacion y creacion

--- src/main/java/com/guardias/backend/controller/FeriadoController.java
+++ src/main/java/com/guardias/backend/controller/FeriadoController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -98,7 +99,7 @@ public class FeriadoController {
         return respuestaValidaciones;
     }
 
-    @PostMapping("/update")
+    @PutMapping(("/update/{id}"))
     public ResponseEntity<?> update(@PathVariable("id") Long id, @RequestBody FeriadoDto feriadoDto) {
         if (!feriadoService.existsById(id))
             return new ResponseEntity(new Mensaje("El feriado no existe"), HttpStatus.BAD_REQUEST);
