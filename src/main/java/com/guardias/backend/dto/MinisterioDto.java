e885e4d43a791db34136e6a3f19badb6e037e992
sebcostilla <sebcostilla@gmail.com>
2024-03-08 13:15:12 -0300

    .

--- src/main/java/com/guardias/backend/dto/MinisterioDto.java
+++ src/main/java/com/guardias/backend/dto/MinisterioDto.java
@@ -1,7 +1,9 @@
 package com.guardias.backend.dto;
 
 import java.util.List;
+
 import com.guardias.backend.entity.Ministerio;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -12,7 +14,6 @@ import lombok.EqualsAndHashCode;
 public class MinisterioDto extends EfectorDto {
 
     Ministerio cabecera;
-    private boolean activo;
     List<Ministerio> ministerios;
 
 }
