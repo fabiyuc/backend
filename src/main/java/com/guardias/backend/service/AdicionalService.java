5c0ace44232bc1db6134b4a96ba3bc25523a01bb
sebcostilla <sebcostilla@gmail.com>
2024-03-11 17:31:17 -0300

    .

--- src/main/java/com/guardias/backend/service/AdicionalService.java
+++ src/main/java/com/guardias/backend/service/AdicionalService.java
@@ -2,12 +2,15 @@ package com.guardias.backend.service;
 
 import java.util.List;
 import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 import com.guardias.backend.entity.Adicional;
 import com.guardias.backend.entity.Revista;
 import com.guardias.backend.repository.AdicionalRepository;
+
 import jakarta.persistence.EntityNotFoundException;
 
 @Service
@@ -65,5 +68,4 @@ public class AdicionalService {
 
         adicionalRepository.save(adicional);
     }
-
 }
