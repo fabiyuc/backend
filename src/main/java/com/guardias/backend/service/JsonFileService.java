35112bbe37ac7670e1004054a71fc4a548b8fb7f
sebcostilla <sebcostilla@gmail.com>
2024-06-18 13:14:15 -0300

    JsonFileRepository y JsonFileService

---
+++ src/main/java/com/guardias/backend/service/JsonFileService.java
@@ -0,0 +1,75 @@
+package com.guardias.backend.service;
+
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDateTime;
+import java.util.Base64;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.guardias.backend.entity.JsonFile;
+import com.guardias.backend.repository.JsonFileRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
+public class JsonFileService {
+    @Autowired
+    JsonFileRepository jsonFileRepository;
+
+    public Optional<JsonFile> findById(Long id) {
+        return jsonFileRepository.findById(id);
+    }
+
+    public Boolean existsById(Long id) {
+        return jsonFileRepository.existsById(id);
+    }
+
+    public Optional<List<JsonFile>> findByActivoTrue() {
+        return jsonFileRepository.findByActivoTrue();
+    }
+
+    public List<JsonFile> findAll() {
+        return jsonFileRepository.findAll();
+    }
+
+    public void save(JsonFile jsonFile) {
+        try {
+            jsonFile.setFechaGuardado(LocalDateTime.now());
+            jsonFileRepository.save(jsonFile);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public JsonFile encodeToJson(String json) {
+        try {
+            byte[] encodedJson = Base64.getEncoder().encode(json.getBytes(StandardCharsets.UTF_8));
+
+            JsonFile jsonFile = new JsonFile();
+            jsonFile.setContenido(encodedJson);
+
+            return jsonFile;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String decodeToJson(JsonFile jsonFile) {
+        try {
+            byte[] decodedBytes = Base64.getDecoder().decode(jsonFile.getContenido());
+            return new String(decodedBytes, StandardCharsets.UTF_8);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
+    }
+
+    public void deleteById(Long id) {
+        jsonFileRepository.deleteById(id);
+    }
+}
