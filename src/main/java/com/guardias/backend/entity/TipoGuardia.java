8aeb77fe460a9c8dbf2ecba0f4ed01a98eabc467
fabiyuc <fabianay0024@gmail.com>
2024-10-22 18:57:42 -0300

    subo modificacion para evitar borrar tabla tipoGuardia

--- src/main/java/com/guardias/backend/entity/TipoGuardia.java
+++ src/main/java/com/guardias/backend/entity/TipoGuardia.java
@@ -9,6 +9,8 @@ import com.guardias.backend.enums.TipoGuardiaEnum;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -29,7 +31,9 @@ public class TipoGuardia {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(columnDefinition = "VARCHAR(30)")
+    /* @Column(columnDefinition = "VARCHAR(30)") */
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private TipoGuardiaEnum nombre;
     @Column(columnDefinition = "VARCHAR(80)")
     private String descripcion;
