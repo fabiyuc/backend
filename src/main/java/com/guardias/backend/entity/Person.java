1bc062036bce4372e00d4e7c5664362dec725d3b
beto_lage16@hotmail.com <beto_lage16@hotmail.com>
2025-05-22 01:58:00 -0300

    .

--- src/main/java/com/guardias/backend/entity/Person.java
+++ src/main/java/com/guardias/backend/entity/Person.java
@@ -113,7 +113,7 @@ public abstract class Person {
      * ArrayList<>();
      */
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "persona", cascade = CascadeType.ALL)
-    @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+    @JsonIgnoreProperties({ "hibernateLazyInitializer", "handler", "persona" })
     private List<HabilitacionesGenerales> habilitacionesGenerales = new ArrayList<>();
 
     @Override
