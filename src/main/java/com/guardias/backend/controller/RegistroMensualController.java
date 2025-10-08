17fdaefab69e2a3843debcb505967c22aba8042e
fabiyuc <fabianay0024@gmail.com>
2025-10-08 09:36:55 -0300

    .

--- src/main/java/com/guardias/backend/controller/RegistroMensualController.java
+++ src/main/java/com/guardias/backend/controller/RegistroMensualController.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,7 +16,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.guardias.backend.dto.Mensaje;
@@ -474,7 +472,7 @@ public class RegistroMensualController {
         }
     }
 
-    @GetMapping("/existen-fuera-de-termino/{idEfector}")
+    @GetMapping("/existen-fuera-de-termino/{idEfector}/{fechaActual}")
     public ResponseEntity<Boolean> existenRegistrosFueraDeTermino(
             @PathVariable Long idEfector,
             @PathVariable LocalDate fechaActual) {
