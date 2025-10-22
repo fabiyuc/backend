23822d9ae69bf012824560184950d6b49f07403d
fabiyuc <fabianay0024@gmail.com>
2025-10-22 12:07:07 -0300

    .

--- src/main/java/com/guardias/backend/repository/DdjjRepository.java
+++ src/main/java/com/guardias/backend/repository/DdjjRepository.java
@@ -177,7 +177,7 @@ public interface DdjjRepository extends JpaRepository<Ddjj, Long> {
                         @Param("tipoGuardia") TipoGuardiaEnum tipoGuardia,
                         @Param("estadoDirector") EstadoDdjjEnum estadoDirector);
 
-        @Query("SELECT d.id FROM Ddjjs d WHERE d.efector.id = :efectorId AND d.mes = :mes AND d.anio = :anio AND d.tipoGuardia.nombre = :tipoGuardia AND d.estadoDdjjDirector = :estado")
+        @Query("SELECT d.id FROM Ddjjs d WHERE d.efector.id = :efectorId AND d.mes = :mes AND d.anio = :anio AND d.tipoGuardia.nombre = :tipoGuardia AND d.estadoDdjjDirector = :estado AND d.activo = true")
         Optional<Long> findIdByEfectorIdAndMesAndAnioAndTipoGuardiaAndEstadoDdjjDirector(
                         @Param("efectorId") Long efectorId,
                         @Param("mes") MesesEnum mes,
