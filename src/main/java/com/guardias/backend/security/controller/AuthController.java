62d851c54ba501f76037073bf464d67687c174f0
fabiyuc <fabianay0024@gmail.com>
2026-01-15 12:02:31 -0300

    .

--- src/main/java/com/guardias/backend/security/controller/AuthController.java
+++ src/main/java/com/guardias/backend/security/controller/AuthController.java
@@ -9,7 +9,6 @@ import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
